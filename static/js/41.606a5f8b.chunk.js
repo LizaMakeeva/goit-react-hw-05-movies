fd759367e2be9cda6421ed5d24489043f2f16504
LizaMakeeva <123816991+LizaMakeeva@users.noreply.github.com>
2023-09-20 08:46:41 +0000

    Deploying to gh-pages from @ LizaMakeeva/goit-react-hw-05-movies@f71109696e6a22f3a0cfe466013dfbaaa81965e8 🚀

---
+++ static/js/41.606a5f8b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[41],{6041:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page"})]})}}}]);
+//# sourceMappingURL=41.606a5f8b.chunk.js.map
